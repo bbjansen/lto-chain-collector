b11a004c99087975e11f72afc9b04e7c1cb329cc
bbjansen <bobjansen@pm.me>
2020-09-10 15:38:02 +0200

    fix db mem leak

--- libs/knex.js
+++ libs/knex.js
@@ -1,9 +1,9 @@
 // Copyright (c) 2018-2020, BB Jansen
 //
 // Please see the included LICENSE file for more information.
-'use strict'
+"use strict";
 
-const knex = module.exports = require('knex')({
+const knex = require("knex")({
   client: process.env.DB_TYPE,
   // debug: true,
   // timezone: 'UTC',
@@ -12,18 +12,18 @@ const knex = module.exports = require('knex')({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    charset: 'utf8'
+    charset: "utf8",
   },
   pool: {
     min: 2,
     max: 151,
-//    createTimeoutMillis: 30000,
-//    acquireTimeoutMillis: 100000,
-//    idleTimeoutMillis: 30000,
-//    reapIntervalMillis: 1000,
-//    createRetryIntervalMillis: 1000,
-    propagateCreateError: false
-  }
-})
+    //    createTimeoutMillis: 30000,
+    //    acquireTimeoutMillis: 100000,
+    //    idleTimeoutMillis: 30000,
+    //    reapIntervalMillis: 1000,
+    //    createRetryIntervalMillis: 1000,
+    propagateCreateError: false,
+  },
+});
 
-module.exports = knex
+module.exports = knex;
